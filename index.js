f98ca8ba726733ef3e895c8439003424236eccbd
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2021-12-01 15:03:54 -0300

    Agrega rutas

--- index.js
+++ index.js
@@ -5,6 +5,14 @@ app.get("/", (req, res) => {
   res.send("Hola Mundo");
 });
 
+app.get("/about", (req, res) => {
+  res.send("About me");
+});
+
+app.get("/contact", (req, res) => {
+    res.send("Contact");
+  });
+
 app.listen(3000, () => {
   console.log("Servidor en puerto 3000");
 });
