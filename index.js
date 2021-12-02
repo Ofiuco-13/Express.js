b386e7b692967719f0c14c9fa8c5e37283286537
Emmanuel  Martinez <emmamartinzwc@gmail.com>
2021-12-02 16:23:10 -0300

    Testea métodos http en Postman

--- index.js
+++ index.js
@@ -2,17 +2,21 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hola Mundo");
+  res.send("Peticion GET recibida");
 });
 
-app.get("/about", (req, res) => {
-  res.send("About me");
+app.post("/about", (req, res) => {
+  res.send("Peticion POST recibida");
 });
 
-app.get("/contact", (req, res) => {
-    res.send("Contact");
-  });
+app.put("/contact", (req, res) => {
+  res.send("Peticion PUT recibida");
+});
 
-app.listen(3000, () => {
-  console.log("Servidor en puerto 3000");
+app.delete("/test", (req, res) => {
+  res.send("<h1>Peticion DELETE recibid</h1>");
+});
+
+app.listen(5000, () => {
+  console.log("Servidor funcionando en puerto 5000");
 });
